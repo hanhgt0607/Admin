c1f7e905bdcf9d0a327dd8dda3515156c8c8bf50
hanhhg0607 <hanhgtd00593@fpt.edu.vn>
2018-11-07 23:08:47 +0700

    nothing

---
+++ Migrations/20181106024825_StudentData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication3.Migrations
+{
+    public partial class StudentData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    RollNumber = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<int>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.RollNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Student    {        [Key]        [Display(Name = "Roll Number")]        public string RollNumber { get; set; }
+        [Display(Name = "First Name")]        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]        public string LastName { get; set; }        [Display(Name = "Phone")]        public string Phone { get; set; }
+        [Display(Name = "Birthday")]        public DateTime Birthday { get; set; }
+        [Display(Name = "Gender")]        public Gender Gender { get; set; }
+        [Display(Name = "Address")]        public string Address { get; set; }
+        [Display(Name = "Created At")]        public DateTime CreatedAt { get; set; }
+        [Display(Name = "Updated At")]        public DateTime UpdatedAt { get; set; }
+        [Display(Name = "Status")]        public Status Status { get; set; }        public Student()
+        {            this.CreatedAt = DateTime.Now;            this.UpdatedAt = DateTime.Now;            this.Status = Status.Actived;            this.Gender = Gender.Male;
+        }    }    public enum Status    {        Actived = 0,        Deactived = 1    }    public enum Gender    {        Male = 1,        Female = 0,        Other = 2    }
+}
